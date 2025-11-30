dd4689b4693ef62bd9f2116585ff47cf7dc25efa
Abejandro14 <alejandrotaco899@gmail.com>
2025-11-29 22:21:16 -0500

    Ejercicio1.cpp

---
+++ Ejercicio1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	string ciudades[5] = {"Quito", "Machachi", "Sangolqui", "Ambato", "Riobamba"};
+	
+	cout << "La ciudad en el indice dos es: " << ciudades[2] << endl;
+	return 0;
+}
